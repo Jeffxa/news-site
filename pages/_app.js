8863bd32a8c2592bc4023f2523d27c531be127ff
Jefferson X. Espinoza <jefferx6@gmail.com>
2023-05-02 00:15:39 -0500

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
-import '@/styles/globals.css'
+import "../css/tailwind.css";
+import "../css/base.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
