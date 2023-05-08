bf53b66482f26516abca9264ace931ea119bef6a
Jefferson Espinoza | Live and Invest Overseas <jespinoza@liveandinvestoverseas.com>
2023-05-08 17:02:56 -0500

    Fixed navbar

--- components/Layout.js
+++ components/Layout.js
@@ -1,45 +1,25 @@
-import { AiOutlineBell, AiOutlineUser } from "react-icons/ai";
 import { FaCode } from "react-icons/fa";
 import { FiSearch } from "react-icons/fi";
 import Link from "next/link";
 export default function Layout({ children }) {
   return (
     <div>
-      <div className="px-2 md:px-24 bg-black text-light flex justify-between h-16 fixed top-0 w-full">
+      <div className="px-2 md:px-24 bg-dark text-light flex justify-between h-16 opacity-90 fixed w-full">
         <Link href="/" legacyBehavior>
-          <a className="text-white uppercase text-2xl flex align-baseline px-10 my-auto">
+          <a className="text-white uppercase bg-black text-2xl flex align-baseline px-10 my-auto">
             <FaCode className="inline text-4xl mr-4" /> Code 911
           </a>
         </Link>
         <div className="flex items-center w-1/3 text-lg">
           <input
             type="text"
-            className="bg-black border-b w-full border-white outline-none"
+            className="bg-dark border-b w-full border-white outline-none hover:text-red"
           />
-          <FiSearch className="text-white -ml-8 text-2xl cursor-pointer" />
-        </div>
-        <div className="flex items-center text-2xl">
-          <AiOutlineBell className="mr-2 cursor-pointer text-white hover:text-red-600" />
-          <AiOutlineUser className="mr-2 cursor-pointer text-white hover:text-red-600" />
+          <FiSearch className="-ml-8 text-2xl cursor-pointer hover:text-red" />
         </div>
       </div>
-      {/* Children div content */}
-      <div className="max-w-[1000px] mx-auto">{children}</div>
-      {/* Children div content */}
-      <div
-        className="
-      bg-black
-      text-white 
-      px-2 
-      md:px-24 
-      justify-between 
-      block
-      bottom-0 
-      h-16 
-      w-full
-      ">
-        Footer
-      </div>
+      {children}
+      <div className="h-16 bg-dark text-light sticky bottom-0">Footer</div>
     </div>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Layout from "/components/Layout";
+import Layout from "/components/Layout";
 
 export default function Home() {
   return (
@@ -9,28 +10,68 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <Layout>
-        <div className="h-20"></div>
-        <h1 className="text-4xl text-center my-8">Hello world from index</h1>
-        <p className="text-xl leading-8 mx-2 text-justify">
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book. It has survived not only
-          five centuries, but also the leap into electronic typesetting,
-          remaining essentially unchanged. It was popularised in the 1960s with
-          the release of Letraset sheets containing Lorem Ipsum passages, and
-          more recently with desktop publishing software like Aldus PageMaker
-          including versions of Lorem Ipsum. <br /> <br />
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book. It has survived not only
-          five centuries, but also the leap into electronic typesetting,
-          remaining essentially unchanged. It was popularised in the 1960s with
-          the release of Letraset sheets containing Lorem Ipsum passages, and
-          more recently with desktop publishing software like Ald
-        </p>
+      <Layout className="flex items-center justify-center h-screen">
+        <div className="min-h-[70px]"></div>
+        <div className="text-xl mx-5 leading-9 max-w-[800px]">
+          <h1 className="text-center text-6xl mb-5">
+            Lorem ipsum dolor sit amet
+          </h1>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
+          sapien sapien, malesuada non massa sit amet, pulvinar pulvinar justo.
+          Vivamus sed suscipit ex, at interdum augue. Proin odio quam, vehicula
+          sed consectetur non, pulvinar non nisl. Maecenas volutpat condimentum
+          turpis. Quisque eu lorem vitae libero viverra interdum eu sit amet
+          enim. Sed dignissim dui id sapien tristique, nec fringilla libero
+          aliquet. Nullam et urna vel orci rhoncus molestie. Aliquam erat
+          volutpat. Mauris congue luctus elementum. Proin vestibulum nisi nec
+          pretium tincidunt. Mauris accumsan augue lectus, vitae ultrices arcu
+          gravida ac. Pellentesque habitant morbi tristique senectus et netus et
+          malesuada fames ac turpis egestas. Cras quis tellus sit amet ligula
+          euismod sodales viverra at neque. Sed vitae libero maximus, viverra
+          erat id, luctus urna. Donec accumsan rutrum facilisis. Class aptent
+          taciti sociosqu ad litora torquent per conubia nostra, per inceptos
+          himenaeos. Donec dolor massa, lobortis sed leo quis, dapibus vehicula
+          elit. Praesent consequat at odio a laoreet. In ultricies placerat
+          risus sit amet tempor. Etiam non ipsum eget turpis sodales iaculis sit
+          amet sed lorem. Ut at lobortis augue. Etiam eget porttitor erat, quis
+          hendrerit ligula. Duis nec purus non nisi gravida cursus a ac augue.
+          Vivamus tempus accumsan vehicula. Phasellus ornare, mauris ut
+          porttitor eleifend, nibh tortor faucibus metus, ut efficitur ante
+          ipsum in massa. Aenean consectetur tempor magna vitae interdum. Ut
+          pulvinar mauris id ex condimentum, id sagittis libero rutrum.
+          Suspendisse cursus consequat tortor, et vulputate ante sodales in.
+          Suspendisse finibus mauris sit amet sollicitudin egestas. Nullam enim
+          risus, consectetur non est sit amet, elementum euismod urna. Nunc ac
+          elit nec ipsum varius hendrerit. <br /> <br />
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
+          sapien sapien, malesuada non massa sit amet, pulvinar pulvinar justo.
+          Vivamus sed suscipit ex, at interdum augue. Proin odio quam, vehicula
+          sed consectetur non, pulvinar non nisl. Maecenas volutpat condimentum
+          turpis. Quisque eu lorem vitae libero viverra interdum eu sit amet
+          enim. Sed dignissim dui id sapien tristique, nec fringilla libero
+          aliquet. Nullam et urna vel orci rhoncus molestie. Aliquam erat
+          volutpat. Mauris congue luctus elementum. Proin vestibulum nisi nec
+          pretium tincidunt. Mauris accumsan augue lectus, vitae ultrices arcu
+          gravida ac. Pellentesque habitant morbi tristique senectus et netus et
+          malesuada fames ac turpis egestas. Cras quis tellus sit amet ligula
+          euismod sodales viverra at neque. Sed vitae libero maximus, viverra
+          erat id, luctus urna. Donec accumsan rutrum facilisis. Class aptent
+          taciti sociosqu ad litora torquent per conubia nostra, per inceptos
+          himenaeos. Donec dolor massa, lobortis sed leo quis, dapibus vehicula
+          elit. Praesent consequat at odio a laoreet. In ultricies placerat
+          risus sit amet tempor. Etiam non ipsum eget turpis sodales iaculis sit
+          amet sed lorem. Ut at lobortis augue. Etiam eget porttitor erat, quis
+          hendrerit ligula. Duis nec purus non nisi gravida cursus a ac augue.
+          Vivamus tempus accumsan vehicula. Phasellus ornare, mauris ut
+          porttitor eleifend, nibh tortor faucibus metus, ut efficitur ante
+          ipsum in massa. Aenean consectetur tempor magna vitae interdum. Ut
+          pulvinar mauris id ex condimentum, id sagittis libero rutrum.
+          Suspendisse cursus consequat tortor, et vulputate ante sodales in.
+          Suspendisse finibus mauris sit amet sollicitudin egestas. Nullam enim
+          risus, consectetur non est sit amet, elementum euismod urna. Nunc ac
+          elit nec ipsum varius{" "}
+          <span className="text-red">Fin del contenido</span>.
+        </div>
       </Layout>
     </div>
   );
