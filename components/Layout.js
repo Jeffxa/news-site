0dd3c11276e23a2e7d7b62608598eb2c5aaf896a
Jefferson X. Espinoza <jefferx6@gmail.com>
2023-05-08 22:46:14 -0500

    Navbar fixed

--- components/Layout.js
+++ components/Layout.js
@@ -1,26 +1,45 @@
-import {FaCode} from "react-icons/fa"
-import {FiSearch} from "react-icons/fi"
-import Link from "next/link"
-export default function Layout ({children})
-{
- return(
+import { AiOutlineBell, AiOutlineUser } from "react-icons/ai";
+import { FaCode } from "react-icons/fa";
+import { FiSearch } from "react-icons/fi";
+import Link from "next/link";
+export default function Layout({ children }) {
+  return (
     <div>
-        <div className="px-2 md:px-24 bg-dark text-light flex justify-between h-16 sticky top-0">
-            <Link href="/" legacyBehavior>
-                <a className="text-white uppercase bg-black text-2xl flex align-baseline px-10 my-auto">
-                    <FaCode className="inline text-4xl mr-4"/> Code 911
-                </a>
-            </Link>
-            <div className="flex items-center w-1/3 text-lg">
-                <input
-                type="text"
-                className="bg-dark border-b w-full border-white outline-none hover:text-red"
-                />
-                <FiSearch className="-ml-8 text-2xl cursor-pointer hover:text-red"/>
-            </div>
+      <div className="px-2 md:px-24 bg-black text-light flex justify-between h-16 fixed top-0 w-full">
+        <Link href="/" legacyBehavior>
+          <a className="text-white uppercase text-2xl flex align-baseline px-10 my-auto">
+            <FaCode className="inline text-4xl mr-4" /> Code 911
+          </a>
+        </Link>
+        <div className="flex items-center w-1/3 text-lg">
+          <input
+            type="text"
+            className="bg-black border-b w-full border-white outline-none"
+          />
+          <FiSearch className="text-white -ml-8 text-2xl cursor-pointer" />
         </div>
-        {children}
-        <div className="h-16 bg-white sticky bottom-0">Footer</div>
+        <div className="flex items-center text-2xl">
+          <AiOutlineBell className="mr-2 cursor-pointer text-white hover:text-red-600" />
+          <AiOutlineUser className="mr-2 cursor-pointer text-white hover:text-red-600" />
+        </div>
+      </div>
+      {/* Children div content */}
+      <div className="max-w-[1000px] mx-auto">{children}</div>
+      {/* Children div content */}
+      <div
+        className="
+      bg-black
+      text-white 
+      px-2 
+      md:px-24 
+      justify-between 
+      block
+      bottom-0 
+      h-16 
+      w-full
+      ">
+        Footer
+      </div>
     </div>
- )   
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import Head from "next/head";
-import Layout from "/components/Layout"
-
+import Layout from "/components/Layout";
 
 export default function Home() {
   return (
@@ -11,9 +10,28 @@ export default function Home() {
       </Head>
 
       <Layout>
-        Creampie dentro del Layout
+        <div className="h-20"></div>
+        <h1 className="text-4xl text-center my-8">Hello world from index</h1>
+        <p className="text-xl leading-8 mx-2 text-justify">
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book. It has survived not only
+          five centuries, but also the leap into electronic typesetting,
+          remaining essentially unchanged. It was popularised in the 1960s with
+          the release of Letraset sheets containing Lorem Ipsum passages, and
+          more recently with desktop publishing software like Aldus PageMaker
+          including versions of Lorem Ipsum. <br /> <br />
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book. It has survived not only
+          five centuries, but also the leap into electronic typesetting,
+          remaining essentially unchanged. It was popularised in the 1960s with
+          the release of Letraset sheets containing Lorem Ipsum passages, and
+          more recently with desktop publishing software like Ald
+        </p>
       </Layout>
-  
     </div>
   );
 }
