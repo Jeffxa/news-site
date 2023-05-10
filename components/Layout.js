d2cd60b8e77af59905d8fe3d789d5afc36fa3dbc
Jefferson Espinoza | Live and Invest Overseas <jespinoza@liveandinvestoverseas.com>
2023-05-10 17:10:55 -0500

    set footer

--- components/Layout.js
+++ components/Layout.js
@@ -1,25 +1,71 @@
 import { FaCode } from "react-icons/fa";
 import { FiSearch } from "react-icons/fi";
+import { AiOutlineBell, AiOutlineUser } from "react-icons/ai";
 import Link from "next/link";
 export default function Layout({ children }) {
   return (
     <div>
-      <div className="px-2 md:px-24 bg-dark text-light flex justify-between h-16 opacity-90 fixed w-full">
+      <div className="px-2 md:px-24 bg-black text-light flex justify-between h-16 opacity-90 fixed w-full">
         <Link href="/" legacyBehavior>
-          <a className="text-white uppercase bg-black text-2xl flex align-baseline px-10 my-auto">
+          <a className="text-white uppercase  text-2xl flex align-baseline px-10 my-auto">
             <FaCode className="inline text-4xl mr-4" /> Code 911
           </a>
         </Link>
         <div className="flex items-center w-1/3 text-lg">
           <input
             type="text"
-            className="bg-dark border-b w-full border-white outline-none hover:text-red"
+            className="bg-black text-white border-b w-full border-white outline-none hover:text-red-600"
           />
-          <FiSearch className="-ml-8 text-2xl cursor-pointer hover:text-red" />
+          <FiSearch className="-ml-8 text-2xl text-white cursor-pointer hover:text-red-600" />
+        </div>
+        <div className="flex items-center text-2xl">
+          <AiOutlineBell className="mr-2 cursor-pointer text-white hover:text-red-600" />
+          <AiOutlineUser className="mr-2 cursor-pointer text-white hover:text-red-600" />
         </div>
       </div>
+      <div className="px-2 md:px-24 pt-20 flex border-b border-black">
+        <Link
+          href="/"
+          className="mr-10 text-xl mt-5 px-5 border-b-2 border-transparent">
+          Home
+        </Link>
+        <Link
+          href="/politics"
+          className="mr-10 text-xl mt-5 px-5 border-b-2 border-transparent">
+          Politics
+        </Link>
+        <Link
+          href="/business"
+          className="mr-10 text-xl mt-5 px-5 border-b-2 border-transparent">
+          Business
+        </Link>
+        <Link
+          href="/sport"
+          className="mr-10 text-xl mt-5 px-5 border-b-2 border-transparent">
+          Sport
+        </Link>
+      </div>
       {children}
-      <div className="h-16 bg-dark text-light sticky bottom-0">Footer</div>
+      <div className="bg-black text-white text-xl py-10 px-24 flex flex-col">
+        <div>
+          <Link href="/" legacyBehavior>
+            <a className="text-white uppercase  text-2xl flex align-baseline px-10 my-auto">
+              <FaCode className="inline text-4xl mr-4" /> Code 911
+            </a>
+          </Link>
+        </div>
+        <div className="grid grid-cols-3 grid-rows-3 gap-5 mt-10 max-w-3xl">
+          <spam>Careers</spam>
+          <spam>About Us</spam>
+          <spam>API</spam>
+          <spam>Media</spam>
+          <spam>Contact Us</spam>
+          <spam>Privacy Policy</spam>
+          <spam>Partners</spam>
+          <spam>Vision</spam>
+          <spam>Project VisQ</spam>
+        </div>
+      </div>
     </div>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import Head from "next/head";
 import Layout from "/components/Layout";
-import Layout from "/components/Layout";
 
 export default function Home() {
   return (
@@ -11,66 +10,70 @@ export default function Home() {
       </Head>
 
       <Layout className="flex items-center justify-center h-screen">
-        <div className="min-h-[70px]"></div>
-        <div className="text-xl mx-5 leading-9 max-w-[800px]">
-          <h1 className="text-center text-6xl mb-5">
+        <div className="text-xl mx-auto max-w-[900px]">
+          <h1 className="text-center text-6xl my-20">
             Lorem ipsum dolor sit amet
           </h1>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
-          sapien sapien, malesuada non massa sit amet, pulvinar pulvinar justo.
-          Vivamus sed suscipit ex, at interdum augue. Proin odio quam, vehicula
-          sed consectetur non, pulvinar non nisl. Maecenas volutpat condimentum
-          turpis. Quisque eu lorem vitae libero viverra interdum eu sit amet
-          enim. Sed dignissim dui id sapien tristique, nec fringilla libero
-          aliquet. Nullam et urna vel orci rhoncus molestie. Aliquam erat
-          volutpat. Mauris congue luctus elementum. Proin vestibulum nisi nec
-          pretium tincidunt. Mauris accumsan augue lectus, vitae ultrices arcu
-          gravida ac. Pellentesque habitant morbi tristique senectus et netus et
-          malesuada fames ac turpis egestas. Cras quis tellus sit amet ligula
-          euismod sodales viverra at neque. Sed vitae libero maximus, viverra
-          erat id, luctus urna. Donec accumsan rutrum facilisis. Class aptent
-          taciti sociosqu ad litora torquent per conubia nostra, per inceptos
-          himenaeos. Donec dolor massa, lobortis sed leo quis, dapibus vehicula
-          elit. Praesent consequat at odio a laoreet. In ultricies placerat
-          risus sit amet tempor. Etiam non ipsum eget turpis sodales iaculis sit
-          amet sed lorem. Ut at lobortis augue. Etiam eget porttitor erat, quis
-          hendrerit ligula. Duis nec purus non nisi gravida cursus a ac augue.
-          Vivamus tempus accumsan vehicula. Phasellus ornare, mauris ut
-          porttitor eleifend, nibh tortor faucibus metus, ut efficitur ante
-          ipsum in massa. Aenean consectetur tempor magna vitae interdum. Ut
-          pulvinar mauris id ex condimentum, id sagittis libero rutrum.
-          Suspendisse cursus consequat tortor, et vulputate ante sodales in.
-          Suspendisse finibus mauris sit amet sollicitudin egestas. Nullam enim
-          risus, consectetur non est sit amet, elementum euismod urna. Nunc ac
-          elit nec ipsum varius hendrerit. <br /> <br />
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
-          sapien sapien, malesuada non massa sit amet, pulvinar pulvinar justo.
-          Vivamus sed suscipit ex, at interdum augue. Proin odio quam, vehicula
-          sed consectetur non, pulvinar non nisl. Maecenas volutpat condimentum
-          turpis. Quisque eu lorem vitae libero viverra interdum eu sit amet
-          enim. Sed dignissim dui id sapien tristique, nec fringilla libero
-          aliquet. Nullam et urna vel orci rhoncus molestie. Aliquam erat
-          volutpat. Mauris congue luctus elementum. Proin vestibulum nisi nec
-          pretium tincidunt. Mauris accumsan augue lectus, vitae ultrices arcu
-          gravida ac. Pellentesque habitant morbi tristique senectus et netus et
-          malesuada fames ac turpis egestas. Cras quis tellus sit amet ligula
-          euismod sodales viverra at neque. Sed vitae libero maximus, viverra
-          erat id, luctus urna. Donec accumsan rutrum facilisis. Class aptent
-          taciti sociosqu ad litora torquent per conubia nostra, per inceptos
-          himenaeos. Donec dolor massa, lobortis sed leo quis, dapibus vehicula
-          elit. Praesent consequat at odio a laoreet. In ultricies placerat
-          risus sit amet tempor. Etiam non ipsum eget turpis sodales iaculis sit
-          amet sed lorem. Ut at lobortis augue. Etiam eget porttitor erat, quis
-          hendrerit ligula. Duis nec purus non nisi gravida cursus a ac augue.
-          Vivamus tempus accumsan vehicula. Phasellus ornare, mauris ut
-          porttitor eleifend, nibh tortor faucibus metus, ut efficitur ante
-          ipsum in massa. Aenean consectetur tempor magna vitae interdum. Ut
-          pulvinar mauris id ex condimentum, id sagittis libero rutrum.
-          Suspendisse cursus consequat tortor, et vulputate ante sodales in.
-          Suspendisse finibus mauris sit amet sollicitudin egestas. Nullam enim
-          risus, consectetur non est sit amet, elementum euismod urna. Nunc ac
-          elit nec ipsum varius{" "}
-          <span className="text-red">Fin del contenido</span>.
+
+          <p className="text-xl md:text-2xl mx-4  pb-10 leading-9 text-justify">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
+            sapien sapien, malesuada non massa sit amet, pulvinar pulvinar
+            justo. Vivamus sed suscipit ex, at interdum augue. Proin odio quam,
+            vehicula sed consectetur non, pulvinar non nisl. Maecenas volutpat
+            condimentum turpis. Quisque eu lorem vitae libero viverra interdum
+            eu sit amet enim. Sed dignissim dui id sapien tristique, nec
+            fringilla libero aliquet. Nullam et urna vel orci rhoncus molestie.
+            Aliquam erat volutpat. Mauris congue luctus elementum. Proin
+            vestibulum nisi nec pretium tincidunt. Mauris accumsan augue lectus,
+            vitae ultrices arcu gravida ac. Pellentesque habitant morbi
+            tristique senectus et netus et malesuada fames ac turpis egestas.
+            Cras quis tellus sit amet ligula euismod sodales viverra at neque.
+            Sed vitae libero maximus, viverra erat id, luctus urna. Donec
+            accumsan rutrum facilisis. Class aptent taciti sociosqu ad litora
+            torquent per conubia nostra, per inceptos himenaeos. Donec dolor
+            massa, lobortis sed leo quis, dapibus vehicula elit. Praesent
+            consequat at odio a laoreet. In ultricies placerat risus sit amet
+            tempor. Etiam non ipsum eget turpis sodales iaculis sit amet sed
+            lorem. Ut at lobortis augue. Etiam eget porttitor erat, quis
+            hendrerit ligula. Duis nec purus non nisi gravida cursus a ac augue.
+            Vivamus tempus accumsan vehicula. Phasellus ornare, mauris ut
+            porttitor eleifend, nibh tortor faucibus metus, ut efficitur ante
+            ipsum in massa. Aenean consectetur tempor magna vitae interdum. Ut
+            pulvinar mauris id ex condimentum, id sagittis libero rutrum.
+            Suspendisse cursus consequat tortor, et vulputate ante sodales in.
+            Suspendisse finibus mauris sit amet sollicitudin egestas. Nullam
+            enim risus, consectetur non est sit amet, elementum euismod urna.
+            Nunc ac elit nec ipsum varius hendrerit. <br /> <br />
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
+            sapien sapien, malesuada non massa sit amet, pulvinar pulvinar
+            justo. Vivamus sed suscipit ex, at interdum augue. Proin odio quam,
+            vehicula sed consectetur non, pulvinar non nisl. Maecenas volutpat
+            condimentum turpis. Quisque eu lorem vitae libero viverra interdum
+            eu sit amet enim. Sed dignissim dui id sapien tristique, nec
+            fringilla libero aliquet. Nullam et urna vel orci rhoncus molestie.
+            Aliquam erat volutpat. Mauris congue luctus elementum. Proin
+            vestibulum nisi nec pretium tincidunt. Mauris accumsan augue lectus,
+            vitae ultrices arcu gravida ac. Pellentesque habitant morbi
+            tristique senectus et netus et malesuada fames ac turpis egestas.
+            Cras quis tellus sit amet ligula euismod sodales viverra at neque.
+            Sed vitae libero maximus, viverra erat id, luctus urna. Donec
+            accumsan rutrum facilisis. Class aptent taciti sociosqu ad litora
+            torquent per conubia nostra, per inceptos himenaeos. Donec dolor
+            massa, lobortis sed leo quis, dapibus vehicula elit. Praesent
+            consequat at odio a laoreet. In ultricies placerat risus sit amet
+            tempor. Etiam non ipsum eget turpis sodales iaculis sit amet sed
+            lorem. Ut at lobortis augue. Etiam eget porttitor erat, quis
+            hendrerit ligula. Duis nec purus non nisi gravida cursus a ac augue.
+            Vivamus tempus accumsan vehicula. Phasellus ornare, mauris ut
+            porttitor eleifend, nibh tortor faucibus metus, ut efficitur ante
+            ipsum in massa. Aenean consectetur tempor magna vitae interdum. Ut
+            pulvinar mauris id ex condimentum, id sagittis libero rutrum.
+            Suspendisse cursus consequat tortor, et vulputate ante sodales in.
+            Suspendisse finibus mauris sit amet sollicitudin egestas. Nullam
+            enim risus, consectetur non est sit amet, elementum euismod urna.
+            Nunc ac elit nec ipsum varius{" "}
+            <span className="text-red-600">Fin del contenido</span>.
+          </p>
         </div>
       </Layout>
     </div>
